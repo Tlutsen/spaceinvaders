3bc706bf6277827e792ee59826bad34a8b8ca947
Thomas Lutsen <thomas.lutsen1625@gmail.com>
2018-05-07 10:47:36 +0200

    Premier vrai commit

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/utils/HorsEspaceJeuException.java
@@ -0,0 +1,7 @@
+package fr.unilim.iut.spaceinvaders.utils;
+
+public class HorsEspaceJeuException extends RuntimeException {
+    public HorsEspaceJeuException(String message) {
+        super(message);
+    }
+}
